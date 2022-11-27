7ce39407d0466c0c2e1f157056db7f212fa4245b
Easter-Maxwell-01 <maxwelleaster00@gmail.com>
2022-11-27 12:41:59 +0100

    Added File : Multiplication Tables

---
+++ times-tables.cpp
@@ -0,0 +1,39 @@
+/*
+    Author : Easter Maxwell
+    Date : 27th November, 2022
+*/
+/*
+	Program : Multiplication tables
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int index;
+	int limit = 13;
+	int number_to_multiply;
+
+    cout << "\n";
+
+	cout << "-[:]-Enter a number to multiply : ";
+	cin  >> number_to_multiply;
+
+    cout << "\n";
+
+	cout << " Multiplication       Result" << endl;
+	cout << "------- --------     --------" << endl;
+
+	for (index = 0; index < limit; index++)
+	{
+		cout << "    " << number_to_multiply << " " << " * " << " " 
+             << index << "\t\t" << number_to_multiply * index;
+
+		cout << "\n";
+	}
+
+	return -1;
+
+}
