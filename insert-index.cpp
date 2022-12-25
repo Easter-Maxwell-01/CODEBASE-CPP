38ecb3cae8785b136d9bcdd841004cfbba675322
Easter Maxwell <115406798+Easter-Maxwell-01@users.noreply.github.com>
2022-12-25 22:20:42 +0100

    Update insert-index.cpp

--- insert-index.cpp
+++ insert-index.cpp
@@ -2,11 +2,12 @@
 	Author : Easter Maxwell
 	Date : 24th December, 2022
 
-    change the element of an index in an array
-    deletes any element set to zero by default
+	change the element of an index in an array
+	deletes any element set to zero by default
 */
 
 #include <iostream>
+#include <windows.h>
 
 #define DELETED   0
 
@@ -16,24 +17,26 @@ const int elements = 5;
 
 int main()
 {
+	int index;
 	int array_input[elements];
 	int int_array[elements] = { 1, 2, 3, 4, 5 };
 
-	for (int index = 0; index < elements; index++)
+	for (index = 0; index < elements; index++)
 	{
-		cout << "Array index [" << (index + 0) << "] = "
-		     << int_array[index] << endl;
+		cout << "`--> Array index [" << (index + 0) << "] = "
+			 << int_array[index] << endl;
 	}
 
 	cout << endl;
 
-	for (int index = 0; index < elements; index++)
+	for (index = 0; index < elements; index++)
 	{
 		cout << "New value for index ["
 			 << (index) << "] : ";
 
-		cin  >> array_input[index];
+		cin >> array_input[index];
 
+		/*delete 0 value elements*/
 		if (array_input[index] == 0)
 		{
 			array_input[index] = DELETED;
@@ -42,11 +45,18 @@ int main()
 
 	cout << endl;
 
-	cout << "---------------------------";
+	string loader = "---------------------------";
+
+	for (index = 0; index < loader.length(); index++)
+	{
+		cout << loader[index];
+
+		Sleep(100);
+	}
 
 	cout << endl << endl;
 
-	for (int index = 0; index < elements; index++)
+	for (index = 0; index < elements; index++)
 	{
 		if (array_input[index] != DELETED)
 		{
@@ -54,6 +64,11 @@ int main()
 				 << "] : " << array_input[index + 0] << endl;
 		}
 	}
+
+	cout << endl;
+	cout << "#Auto deleted elements of 0 value!";
+
+	cout << endl;
 
 	return 0;
 
